e8ab14435c859dd41dedf743c337bfedb33033b5
superyarick <yarick@yarick.com>
2017-12-04 19:46:14 -0500

    Adding check for aws S3 bucket writable to the world

--- controls/jumpb.rb
+++ controls/jumpb.rb
@@ -1,16 +1,3 @@
-# encoding: utf-8
-#
-=begin
------------------
-AWS-INSPEC-DEMO
-Release Date: 2017-12-04
-Version: 0.1
-Publisher: MITRE
-Source: MITRE.ORG
-uri: http://mitre.org
------------------
-=end
-
 
 control 'sg-1' do
   impact 1.0
@@ -31,24 +18,18 @@ end
 
 # playbook to setup and config s3 bucket
 
-
-control 's3bucket' do
+control "s3bucket" do
   impact 0.7
-  title 'test s3 bucket thing'
+  title "test s3 bucket thing"
   desc "what we want to test on s3 buckets"
 
-  tag nist: "AC-7"
+  tag "nist": "AC-7"
 
-  # perhaps this cli call is asking 'is the bucket publiclly accessable'
-  # I think the cli call here would be 'has public set to yes'
+  buckets = inspec.command("aws s3api list-buckets --query 'Buckets[].Name' --output text").stdout.strip.lines
 
-  # buckets = list
-  buckets = inspec.command('aws s3api list-buckets --query "Buckets[].Name" --output text').stdout.strip
-
-  buckets.each do | bucket |
-  results = inspec.command("aws s3api get-bucket-acl --bucket #{bucket} --query Grants[?Grantee.Type==\'Group\'].[Grantee.URI,Permission] --output text | awk '{ print $1 }'").stdout.strip
-
-   describe results do
-     its(content) { should_not include %r{AllUsers|AuthenticatedUsers} }
+  buckets.each do |bucket|
+    results = inspec.command("aws s3api get-bucket-acl --bucket #{bucket} --query Grants[?Grantee.Type==\'Group\'].[Grantee.URI,Permission] --output text | awk '{ print $1 }'").stdout.strip
+     describe results do
+       its(content) { should_not include %r{AllUsers|AuthenticatedUsers} }
+     end
    end
- end
