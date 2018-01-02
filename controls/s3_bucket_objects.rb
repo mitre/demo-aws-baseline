b5a17ec9540b28acc4440b1649e42e0b745b618e
Aaron Lippold <lippold@gmail.com>
2018-01-02 10:45:31 -0500

    * updated demo controls per Matt's new work * added in needed descriptions for ingress tests * moved old code out of the way
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/s3_bucket_objects.rb
+++ controls/s3_bucket_objects.rb
@@ -8,33 +8,13 @@ control "s3bucket-public-objects" do
   tag "check": ""
   tag "fix": ""
 
-  # #TODO need to decide which is the right approach
-  # my_buckets = inspec.aws_s3_buckets(vpc: #{vpc_id})
-  # *or*
-  # my_buckets = inspec.aws_s3_buckets
-  # my_buckets.each do |bucket|
-  #   describe aws_s3_bucket_objects(bucket) do
-  #     it { should_not have_public_objects }
-  #     #TODO need to decide which approach makes the most sense
-  #     its('objects.public') { should cmp [] }
-  #     *or*
-  #     its('objects.pubic') { should be nil }
-  #     its('objects.private.count') { should be > 0 }
-  #   end.only_if(bucket.entries.count > 0 )
-  #   # MY FIRST INSTINCT: `bucket.objects.count`
-  #   # MIDDLE GROUND?: make `objects` and alias for `entires`
-  #   # or we could use `bucket.entries` like security_group does.
-  #   # NOTE: Keeping things consistent across resources makes sense.
-  #   # BUT: Only if we think it will help the user experience
-  #   # the private var seems to be `bucket_objects` ...
-  # end
-  # # short and sweet version
-  # my_buckets.each do |bucket|
-  #  describe aws_s3_bucket_objects(bucket) do
-  #    # returns a true or false
-  #    it { should_not have_public_objects }
-  #    # returns a list of offenders if fails
-  #    its('objects.public') { should cmp [] }
-  #  end
-  # end
+  my_buckets = inspec.aws_s3_buckets.buckets
+  my_buckets.each do |bucket|
+   describe aws_s3_bucket_objects(bucket) do
+     # returns a true or false
+     it { should_not have_public_objects }
+     # returns a list of offenders if fails
+     its('objects.public') { should cmp [] }
+   end
+  end
 end
