ad9004eae28a898d70c59ebf2fb15f927ade943f
ejaronne <34140975+ejaronne@users.noreply.github.com>
2018-01-02 12:44:09 -0500

    Update with AC-6 nist tag, check and fix language

--- controls/s3_bucket_objects.rb
+++ controls/s3_bucket_objects.rb
@@ -1,12 +1,19 @@
 
-control "s3bucket-public-objects" do
+control "s3-objects-no-public-access" do
   impact 0.7
-  title "Ensure there are no Publicly Accessable S3 objects"
-  desc "Ensure there are no Publicly Accessable S3 objects"
-  tag "nist": ["CM-7", "Rev_4"]
+  title "Ensure there are no publicly accessable S3 objects"
+  desc "Ensure there are no publicly accessable S3 objects"
+  
+  tag "nist": ["AC-6", "Rev_4"]
   tag "severity": "high"
-  tag "check": ""
-  tag "fix": ""
+  
+  tag "check": ""Review your AWS console and note if any S3 objects are set to
+                'Public'. If any objects are listed as 'Public', then this is
+                a finding."
+
+  tag "fix": "Log into your AWS console and select the S3 objects section. Select
+              the objects found in your review. Select the permisssions tab for
+              the objects and remove the Public access permission."
 
   my_buckets = inspec.aws_s3_buckets.buckets
   my_buckets.each do |bucket|
