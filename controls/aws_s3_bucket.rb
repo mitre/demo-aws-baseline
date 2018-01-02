25c6be5bb80a57449843f84076ec706d78973961
ejaronne <34140975+ejaronne@users.noreply.github.com>
2018-01-02 12:24:25 -0500

    Update aws_s3_bucket.rb

--- controls/aws_s3_bucket.rb
+++ controls/aws_s3_bucket.rb
@@ -9,23 +9,21 @@ fixtures = {}
   )
 end
 
-# research checking for 'http' proto from the world
-
 control "s3-buckets-no-public-access" do
   impact 0.7
   title "Ensure there are no publicly accessable S3 Buckets"
   desc "Ensure there are no publicly accessable S3 Buckets"
 
-  tag "nist": ["CM-6", "Rev_4"]
+  tag "nist": ["AC-6", "Rev_4"]
   tag "severity": "high"
 
   tag "check": "Review your AWS console and note if any S3 buckets are set to
                 'Public'. If any buckets are listed as 'Public', then this is
                 a finding."
 
-  tag "fix": "Log into your AWS console and select the S3 buckts section. Select
+  tag "fix": "Log into your AWS console and select the S3 buckets section. Select
               the buckets found in your review. Select the permisssions tab for
-              the bucket and remove the Public Access permission."
+              the bucket and remove the Public access permission."
 
   describe aws_s3_buckets do
     it { should_not have_public_buckets }
