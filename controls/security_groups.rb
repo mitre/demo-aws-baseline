c9a4b11a0cfc979317f2fbb647550b983271eb75
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2018-01-04 16:31:00 -0500

    Changes egress and ingress test to show what conflicting rules are found
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

--- controls/security_groups.rb
+++ controls/security_groups.rb
@@ -123,4 +123,11 @@ control "cis_aws_foundations-4.4" do
       its('ingress_rules.count') { should eq 0 }
       its('egress_rules.count') { should eq 0 }
     end
+
+    # You should be able to find the security group named default
+    describe aws_ec2_security_group(group_id: fixtures['ec2_security_group_default_group_id']) do
+      it { should exist }
+      its('ingress_rules') { should cmp [] }
+      its('egress_rules')  { should cmp [] }
+    end
   end
