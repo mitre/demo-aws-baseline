8e94dc1657448f7b7f14167616cc3821640adf41
ejaronne <34140975+ejaronne@users.noreply.github.com>
2018-01-04 09:38:39 -0500

    Corrected 4.1 check text to match CIS benchmark language for port 22; changed control to SC-7(5) System and Communications Protection -Deny by Default/Allow by Exception

--- controls/security_groups.rb
+++ controls/security_groups.rb
@@ -42,7 +42,7 @@ control "cis_aws_foundations-4.1" do
   desc "Security groups provide stateful filtering of ingress/egress network
         traffic to AWS resources. It is recommended that no security group allows unrestricted
         ingress access to port 22."
-  tag "nist": ["AC-17","Rev_4"]
+  tag "nist": ["SC-7(5)","Rev_4"]
   tag "severity": "high"
 
   tag "check": "
@@ -51,9 +51,9 @@ control "cis_aws_foundations-4.1" do
       3. For each security group, perform the following:
         1. Select the security group
         2. Click the Inbound Rules tab
-        3. Ensure no rule exists that has a port range that includes port 3389 and
+        3. Ensure no rule exists that has a port range that includes port 22 and
            has a Source of 0.0.0.0/0 Note: A Port value of ALL or a port range
-           such as 1024-4098 are inclusive of port 3389. "
+           such as 0-1024 are inclusive of port 22. "
   tag "fix": "
       1. Login to the AWS Management Console at https://console.aws.amazon.com/vpc/home
       2. In the left pane, click Security Groups
@@ -75,7 +75,7 @@ control "cis_aws_foundations-4.2" do
   desc "Security groups provide stateful filtering of ingress/egress network traffic
         to AWS resources. It is recommended that no security group allows unrestricted
         ingress access to port 3389."
-  tag "nist": ["AC-17","Rev_4"]
+  tag "nist": ["SC-7(5)","Rev_4"]
   tag "severity": "high"
   tag "check": "Perform the following to determine if the account is configured as prescribed:
       1. Login to the AWS Management Console at https://console.aws.amazon.com/vpc/home
@@ -105,7 +105,7 @@ control "cis_aws_foundations-4.4" do
         region should have it's default security group updated to comply.  Any newly
         created VPCs will automatically contain a default security group that will
         need remediation to comply with this recommendation. "
-  tag "nist": ["CM-7","Rev_4"]
+  tag "nist": ["SC-7(5)","Rev_4"]
   tag "severity": "high"
   tag "check": "Perform the following to determine if the account is configured as prescribed:
         Security Group State
